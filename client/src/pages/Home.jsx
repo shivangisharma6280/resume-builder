f5c005daab9a688d673953f33a9c21a58ab0fa8a
Shivangi Sharma <shivangisharma6280@gmail.com>
2026-07-17 20:20:11 +0530

    Fix import case issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../components/home/banner'
+import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial'
